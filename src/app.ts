c9f5d041b080c6fa18d786187302c9969b37913a
wgeiger <wgeiger@cleo.com>
2019-04-14 13:59:47 -0400

    Simple function example

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log('Ahoy! TypeScript!');
+const pizzas =[
+    {name:'pepperoni', toppings: ['pepperoni'] }
+];
+
+const mappedPizzas = pizzas.map(function (pizza) {
+    return pizza.name.toUpperCase()
+});
+
+console.log(mappedPizzas);
