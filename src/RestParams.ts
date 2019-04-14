94e169aa3014a16eefc3ba7c8ebd4844faefdf7c
wgeiger <wgeiger@cleo.com>
2019-04-14 14:29:08 -0400

    ES6 rest parameters

---
+++ src/RestParams.ts
@@ -0,0 +1,15 @@
+function sumAll(arr: number[]){
+    return arr.reduce((prev,next)=>prev+next);
+}
+
+const sum = sumAll([1,2,3,4,5,6,7,8,9,10])
+console.log(sum);
+
+//or
+
+function sumAll2(... arr: number[]){
+    return arr.reduce((prev,next)=>prev+next);
+}
+
+const sum2 = sumAll2(1,2,3,4,5,6,7,8,9,10)
+console.log(sum2);
